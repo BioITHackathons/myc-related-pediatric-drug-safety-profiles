22bbc90c114b006883c0ce99862936085aa636a8
Amirhossein Manzourolajdad <amir.manzour@gmail.com>
2023-05-15 15:55:17 -0400

    metadata added

--- R/myc_related_drug_safety_data.R
+++ R/myc_related_drug_safety_data.R
@@ -20,3 +20,26 @@ tbl(con,"ade_nichd_enrichment") %>%
     )) %>% 
     collect() %>% 
     arrow::write_feather("data/myc_related_drug_safety_enrichment_data.feather")
+
+mydrugs= "";
+for (i in 1:length(myc_related_drugs)){
+    mydrugs = paste(mydrugs,",",myc_related_drugs[i],sep="")
+}
+mydrugs=substring(mydrugs,2,nchar(mydrugs))
+query <- paste("select drug.atc_concept_id,drug.atc_concept_name,drug.atc4_concept_name,
+drug.atc3_concept_name,drug.atc2_concept_name,drug.atc1_concept_name,
+ade_nichd.meddra_concept_id, ade_nichd.ade_name,ade_nichd.nichd,
+event.meddra_concept_name_4, event.meddra_concept_name_3,
+event.meddra_concept_name_2,event.meddra_concept_name_1
+from drug inner join ade_nichd on drug.atc_concept_id = ade_nichd.atc_concept_id
+inner join event on event.meddra_concept_id = ade_nichd.meddra_concept_id
+where drug.atc_concept_id in (", mydrugs,")",sep="");
+
+#dplyr::tbl(con, dplyr::sql(query)) %>% collect() %>% View();
+
+mytable<-dplyr::tbl(con, dplyr::sql(query)) %>% collect()
+
+arrow::write_feather(mytable, paste("data/",
+                                    "myc_related_drug_safety_metadata",".feather", sep = ""))
+
+kidsides::disconnect_sqlite_db(con)
